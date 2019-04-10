765c3afada36d49997dca3c05de9e812d8700f85
isaac1104 <isaac-kwon@hotmail.com>
2019-04-10 18:11:47 +0900

    change api data limit to 50

--- routes/saved_tracks_api.js
+++ routes/saved_tracks_api.js
@@ -4,7 +4,7 @@ module.exports = app => {
   app.get('/api/saved_tracks', async (req, res) => {
     try {
       const { accessToken } = req.user;
-      const request = await axios.get('https://api.spotify.com/v1/me/tracks?market=US', { headers: { Authorization: `Bearer ${accessToken}` } });
+      const request = await axios.get('https://api.spotify.com/v1/me/tracks?market=US&limit=50', { headers: { Authorization: `Bearer ${accessToken}` } });
       const { data } = request;
       res.status(200).send(data);
     } catch (e) {
