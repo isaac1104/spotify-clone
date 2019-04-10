2a4bdfafd7f5b716e27d22d17f5c9999abc2ee58
isaac1104 <isaac-kwon@hotmail.com>
2019-04-10 16:22:32 +0900

    decorate text on Landing.js

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -13,14 +13,17 @@ const styles = {
     textAlign: 'center'
   },
   text: {
-    color: '#ffffff'
+    color: '#1db954'
+  },
+  strike: {
+    color: 'tomato'
   }
 };
 
 const Landing = () => (
   <div style={styles.container}>
     <div>
-      <Title style={styles.text}>Welcome to Spotify Clone</Title>
+      <Title level={2} style={styles.text}>Welcome to Spotify <strike style={styles.strike}>Clone</strike></Title>
       <Button href='/auth/spotify'>Sign in with Spotify</Button>
     </div>
   </div>
